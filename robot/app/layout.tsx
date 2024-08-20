195794a3ed106d68efcc85ae1cb4fd54ffb1a6d9
reneafranco <reneadonayfranco@gmail.com>
2024-08-20 16:56:09 -0400

    initial state

---
+++ robot/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+const JetBrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbrainsMono",
+});
+
+export const metadata: Metadata = {
+  title: "Rene's Portfolio",
+  description: "Rene A Franco Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={JetBrainsMono.variable}>
+        {" "}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ robot/app/page.tsx
@@ -0,0 +1,16 @@
+// pages/index.js
+import Spline from "@splinetool/react-spline";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Mi Proyecto Spline en Next.js</h1>
+      <Spline
+        scene="https://prod.spline.design/iVFfKSjg8whvJclY/scene.splinecode"
+        style={{ width: "100%", height: "100vh", index = "-1" }}
+      />
+
+      <h1> HELLO HHEY EYYEY </h1>
+    </main>
+  );
+}
