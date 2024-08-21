a8094010ac63deebfcc8f48d40abd39fe431fa31
reneafranco <reneadonayfranco@gmail.com>
2024-08-20 20:58:09 -0400

    animations

--- robot/components/Hero.tsx
+++ robot/components/Hero.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import Spline from "@splinetool/react-spline/next";
 const Hero = () => {
   return (
-    <div className="h-screen w-full pt-36">
-      <div className="pb-5">
-        <Spline scene="https://prod.spline.design/iVFfKSjg8whvJclY/scene.splinecode" />
+    <div className="relative h-screen w-full pt-36">
+      <div>
+        <div className="absolute inset-0 z-10">
+          <Spline scene="https://prod.spline.design/iVFfKSjg8whvJclY/scene.splinecode" />
+        </div>
+        <div className="flex justify-center items-center">
+          <h1> Rene Project</h1>
+        </div>
       </div>
     </div>
   );
