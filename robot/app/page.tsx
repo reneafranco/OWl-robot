2c728faca714fa065bca2a64568737c8c8fb25be
reneafranco <reneadonayfranco@gmail.com>
2024-08-20 21:15:31 -0400

    coomit sss

--- robot/app/page.tsx
+++ robot/app/page.tsx
@@ -7,7 +7,7 @@ import Spline from "@splinetool/react-spline";
 export default function Home() {
   return (
     <main
-      lassName="relative bg-black-100 flex justify-center
+      className="relative bg-black-100 flex justify-center
     items-center flex-col mx-auto sm:px-10 px-5 overflow-clip"
     >
       <div className="max-w7xl w-full">
