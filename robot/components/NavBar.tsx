d095b74ec8d9d0cc64ccb22584a193e7dd9707f5
reneafranco <reneadonayfranco@gmail.com>
2024-08-24 12:29:39 -0400

    dark mode shadcn stage 1 ok

--- robot/components/NavBar.tsx
+++ robot/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { ModeToggle } from "./model-toggle";
 
 const NavBar = () => {
   return (
     <div>
-      <header className="body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <header className="body-font ">
+        <div className="sticky top-0 container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a className="flex title-font font-medium items-center mb-4 md:mb-0">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -25,20 +26,12 @@ const NavBar = () => {
             <a className="mr-5 hover:text-gray-900">Second Link</a>
             <a className="mr-5 hover:text-gray-900">Third Link</a>
           </nav>
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            Button
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-4 h-4 ml-2"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
+          <div
+            className="inline-flex items-center  border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 
+          rounded text-base mt-4 md:mt-0"
+          >
+            <ModeToggle />
+          </div>
         </div>
       </header>
     </div>
