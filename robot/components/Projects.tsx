acfb3d7af3ef3f5c02dcf8a69a94940329f1179e
reneafranco <reneadonayfranco@gmail.com>
2024-08-24 11:52:58 -0400

    projects links ok

--- robot/components/Projects.tsx
+++ robot/components/Projects.tsx
@@ -22,7 +22,7 @@ const Projects = () => {
                 alt="content"
               />
               <h3 className="tracking-widest text-blue-500 text-xs font-medium title-font">
-                SUBTITLE
+                TITLE
               </h3>
               <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
                 Chichen Itza
@@ -31,63 +31,71 @@ const Projects = () => {
                 Fingerstache flexitarian street art 8-bit waistcoat. Distillery
                 hexagon disrupt edison bulbche.
               </p>
-            </div>
-          </div>
-          <div className="xl:w-1/4 md:w-1/2 p-4">
-            <div className="bg-gray-100 p-6 rounded-lg">
-              <img
-                className="h-40 rounded w-full object-cover object-center mb-6"
-                src="https://dummyimage.com/721x401"
-                alt="content"
-              />
-              <h3 className="tracking-widest text-blue-500 text-xs font-medium title-font">
-                SUBTITLE
-              </h3>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                Colosseum Roma
-              </h2>
-              <p className="leading-relaxed text-base">
-                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                hexagon disrupt edison bulbche.
-              </p>
-            </div>
-          </div>
-          <div className="xl:w-1/4 md:w-1/2 p-4">
-            <div className="bg-gray-100 p-6 rounded-lg">
-              <img
-                className="h-40 rounded w-full object-cover object-center mb-6"
-                src="https://dummyimage.com/722x402"
-                alt="content"
-              />
-              <h3 className="tracking-widest text-blue-500 text-xs font-medium title-font">
-                SUBTITLE
-              </h3>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                Great Pyramid of Giza
-              </h2>
-              <p className="leading-relaxed text-base">
-                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                hexagon disrupt edison bulbche.
-              </p>
-            </div>
-          </div>
-          <div className="xl:w-1/4 md:w-1/2 p-4">
-            <div className="bg-gray-100 p-6 rounded-lg">
-              <img
-                className="h-40 rounded w-full object-cover object-center mb-6"
-                src="https://dummyimage.com/723x403"
-                alt="content"
-              />
-              <h3 className="tracking-widest text-blue-500 text-xs font-medium title-font">
-                SUBTITLE
-              </h3>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                San Francisco
-              </h2>
-              <p className="leading-relaxed text-base">
-                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                hexagon disrupt edison bulbche.
-              </p>
+              <div className="grid grid-cols-6">
+                <div className="col-span-3">
+                  <span className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 mt-4">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-6 h-6"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle cx="6" cy="6" r="3"></circle>
+                      <circle cx="6" cy="18" r="3"></circle>
+                      <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                    </svg>
+                  </span>
+                  <span className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 mt-4 mx-2 ">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-6 h-6"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle cx="6" cy="6" r="3"></circle>
+                      <circle cx="6" cy="18" r="3"></circle>
+                      <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                    </svg>
+                  </span>
+                  <span className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 mt-4 mx-2 ">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-6 h-6"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle cx="6" cy="6" r="3"></circle>
+                      <circle cx="6" cy="18" r="3"></circle>
+                      <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                    </svg>
+                  </span>
+                </div>
+                <div className="col-span-3 mt-5 mx-1">
+                  <a className="text-blue-500 inline-flex items-center mx-8 ">
+                    Learn More
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
