340f9ca828158f9303bf86375dd56ec840fa9533
reneafranco <reneadonayfranco@gmail.com>
2024-08-24 11:16:34 -0400

    About page stage 1

--- robot/app/page.tsx
+++ robot/app/page.tsx
@@ -1,16 +1,19 @@
 // pages/index.js
 import Hero from "@/components/Hero";
 import NavBar from "@/components/NavBar";
+import About from "@/components/About";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
 import Spline from "@splinetool/react-spline";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full overflow-clip">
+    <main className="h-screen w-full">
       <div className="">
         <NavBar />
         <Hero />
+
+        <About />
       </div>
     </main>
   );
