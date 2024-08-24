a75fef156005c2cad25ac3b55b1a2401dcd550cd
reneafranco <reneadonayfranco@gmail.com>
2024-08-24 12:11:06 -0400

    testimonial page stage 1 ok

--- robot/app/page.tsx
+++ robot/app/page.tsx
@@ -4,6 +4,7 @@ import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <Hero />
         <About />
         <Projects />
+        <Testimonials />
         <Contact />
         <Footer />
       </div>
