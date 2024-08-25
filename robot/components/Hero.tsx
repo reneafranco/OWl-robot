c8a53e4f1183d10d20bb51de9ac24d6dcd951624
reneafranco <reneadonayfranco@gmail.com>
2024-08-25 17:55:33 -0400

    hero section

--- robot/components/Hero.tsx
+++ robot/components/Hero.tsx
@@ -4,30 +4,34 @@ import Spline from "@splinetool/react-spline/next";
 const Hero = () => {
   return (
     <div
-      className="grid  min-h-screen sm:grid-cols-12 grid-cols-1  gap-4
+      className="grid  min-h-screen grid-cols-1  gap-4
 
       mx-2"
       id="home"
     >
       <div
-        className="min-h-[100px] sm:col-span-2
-        sm:block hidden"
-      ></div>
-      <div
-        className="relative min-h-[100px] sm:col-span-8 
+        className="relative min-h-[100px] 
         sm:block"
       >
-        <div className="mt-4 relative z-10">
-          <h1>WELCOME DEVELOPER</h1>
+        <div>
+          <div className=" mt-20 flex flex-col items-center text-center">
+            <h1 className="title-font md:text-6xl sm:text-5xl text-4xl font-medium text-gray-900 dark:text-white">
+              Welcome to Iowa
+            </h1>
+            <div className="ml-44">
+              <h1 className="sm:tx-4xl text-3xl text-gray-900 dark:text-white">
+                Welcome to Iowa
+              </h1>
+              <h3 className="md:block hidden text-gray-600 dark:text-white-200 pl-20">
+                Hover the owl head{" "}
+              </h3>
+            </div>
+          </div>
         </div>
         <div className=" absolute top-0 left-0 right-0 bottom-0">
           <Spline scene="https://prod.spline.design/iVFfKSjg8whvJclY/scene.splinecode" />
         </div>
       </div>
-      <div
-        className="min-h-[100px] sm:col-span-2
-        sm:block hidden"
-      ></div>
     </div>
   );
 };
